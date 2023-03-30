0fca6222743a36b4606857a439b97359a4e987c8
lsuazo <luisrsuazo@gmail.com>
2023-03-30 11:23:08 -0500

    Added basic setup files

---
+++ src/cube/state.py
@@ -0,0 +1,5 @@
+class CubeState:
+    def __init__() -> None:
+        # should I have six arrays?
+        # should I represent the pieces themselves, and their positions
+        # like center_red, center_blue,... corner_red_blue_white, ... edge_red_blue ?? and then positions front_0, front_1, ..., left_0, left_1,... etc.
